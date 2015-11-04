934786cd3df0c54e2528f793ab3182396db98f5d
KrasiNedew <krasi.nedew@gmail.com>
2015-11-04 15:09:24 +0200

    Input Handler

---
+++ AirHockey/src/Game/InputHandler.java
@@ -0,0 +1,35 @@
+package Game;
+
+import display.Display;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Created by krasimir on 11/4/2015.
+ */
+public class InputHandler implements KeyListener {
+
+    public InputHandler(Display display) {
+        display.getCanvas().addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+    }
+}
