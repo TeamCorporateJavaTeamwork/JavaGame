d26f73e2d67f0282fbb0a0156b0e71b4b7d61e21
Boyan <boyanklisurov@gmail.com>
2015-11-10 15:09:09 +0200

    menu bug fixed

--- AirHockey/src/Game/MouseInputHandler.java
+++ AirHockey/src/Game/MouseInputHandler.java
@@ -1,5 +1,6 @@
 package Game;
 
+import states.MenuState;
 import states.StateManager;
 
 import java.awt.event.MouseEvent;
@@ -14,13 +15,15 @@ public class MouseInputHandler implements MouseListener{
 		int mouseY = e.getY();
 
 		if(mouseX >= 430 && mouseX <= 770) {
-			if(mouseY >= 150 && mouseY <= 200) {
+			if(mouseY >= 150 && mouseY <= 200 && MenuState.isOn == true) {
+				MenuState.isOn = false;
 				GameEngine.player1.setName(GameEngine.getPlayerName(1));
 				GameEngine.player1.convertNameCharsToKeys();
 				GameEngine.player2.setName(GameEngine.getPlayerName(2));
 				GameEngine.player2.convertNameCharsToKeys();
 				GameEngine.State.setState(StateManager.STATES.GAME);
-			} else if (mouseY >= 350 && mouseY <= 400) {
+			} else if (mouseY >= 350 && mouseY <= 400 && MenuState.isOn == true) {
+				MenuState.isOn = false;
 				System.exit(1);
 			}
 		}
--- AirHockey/src/states/MenuState.java
+++ AirHockey/src/states/MenuState.java
@@ -7,13 +7,9 @@ import java.awt.geom.RoundRectangle2D;
 
 public class MenuState {
 
-	private RoundRectangle2D playButton = createButton(400, 150);
-	private RoundRectangle2D settingsButton = createButton(400, 250);
-	private RoundRectangle2D quitButton = createButton(400, 350);
+	public static boolean isOn = true;
 
 	public void render(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-
 		Font titleFont = new Font("arial", Font.BOLD, 50);
 		g.setFont(titleFont);
 		g.setColor(Color.LIGHT_GRAY);
@@ -27,18 +23,5 @@ public class MenuState {
 		g.drawImage(Assets.playGame, 410, 135, 388, 89, null);
 		g.drawImage(Assets.settings, 410, 235, 388, 89, null);
 		g.drawImage(Assets.quitGame, 410, 335, 388, 89, null);
-		//drawButton(g2d, g, "Play Game", 530, 185, Color.LIGHT_GRAY, playButton);
-		//drawButton(g2d, g, "Settings", 530, 285, Color.GREEN, settingsButton);
-		//drawButton(g2d, g, "Quit Game", 530, 385, Color.RED, quitButton);
-	}
-
-	private RoundRectangle2D createButton(double x, double y) {
-		return new RoundRectangle2D.Double(x, y, 400.0, 50.0, 10.0, 10.0);
-	}
-
-	private void drawButton(Graphics2D g2d, Graphics g, String buttonLabel, int x, int y, Color color, RoundRectangle2D button) {
-		g.setColor(color);
-		g.drawString(buttonLabel, x, y);
-		g2d.draw(button);
 	}
 }
